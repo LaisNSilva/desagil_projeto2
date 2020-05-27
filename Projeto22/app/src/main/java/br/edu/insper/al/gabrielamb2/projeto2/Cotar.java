3ea4768d2fae28b51d35bdb9252a0c01df09b385
Ellen <ellenbs@al.insper.edu.br>
2020-05-27 14:10:05 -0300

    Spinners funcionando

--- Projeto22/app/src/main/java/br/edu/insper/al/gabrielamb2/projeto2/Cotar.java
+++ Projeto22/app/src/main/java/br/edu/insper/al/gabrielamb2/projeto2/Cotar.java
@@ -3,12 +3,34 @@ package br.edu.insper.al.gabrielamb2.projeto2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Cotar extends AppCompatActivity {
+
+    private void showToast(String text) {
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cotar);
+
+        Spinner materiais = findViewById(R.id.material);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.materiais, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        materiais.setAdapter(adapter1);
+
+        Spinner impressoras = findViewById(R.id.impressora);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.impressoras, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        impressoras.setAdapter(adapter2);
+
     }
 }
