b53391f5b736fc34355d40604aceaac5d445199c
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2020-05-27 09:35:39 -0300

    interface iniciada

---
+++ Projeto22/app/src/main/java/br/edu/insper/al/gabrielamb2/projeto2/Cotar.java
@@ -0,0 +1,14 @@
+package br.edu.insper.al.gabrielamb2.projeto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cotar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cotar);
+    }
+}
---
+++ Projeto22/app/src/main/java/br/edu/insper/al/gabrielamb2/projeto2/MainActivity.java
@@ -0,0 +1,57 @@
+package br.edu.insper.al.gabrielamb2.projeto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonCotacao = findViewById(R.id.button_orcamento);
+        Button buttonPedidos = findViewById(R.id.button_orcamento_antigos);
+        Button buttonConfiguracao = findViewById(R.id.button_configuracoes);
+        Button buttonFilamentos = findViewById(R.id.button_tiposdefilamento);
+
+
+        final Intent cotacao = new Intent(this, Cotar.class);
+
+        //Intent pedidos = new Intent(this, Pedidos.class);
+
+       // Intent configuracao = new Intent(this, Configuracao.class);
+
+        //Intent filamentos = new Intent(this, Filamentos.class);
+
+        buttonCotacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(cotacao);
+            }
+        });
+        buttonPedidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // startActivity(pedidos);
+            }
+        });
+        buttonConfiguracao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // startActivity(configuracao);
+            }
+        });
+        buttonFilamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(filamentos);
+            }
+        });
+
+    }
+}
